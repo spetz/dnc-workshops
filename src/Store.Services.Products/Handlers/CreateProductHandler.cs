821a8aac3712b88fc7e737b4d08f1e916f705738
Piotr Gankiewicz <spetz90@gmail.com>
2018-06-30 14:45:53 +0200

    Save product to Mongo

--- src/Store.Services.Products/Handlers/CreateProductHandler.cs
+++ src/Store.Services.Products/Handlers/CreateProductHandler.cs
@@ -4,21 +4,28 @@ using Dnc.Common;
 using Dnc.Common.Commands;
 using Dnc.Common.RabbitMq;
 using Store.Messages.Products;
+using Store.Services.Products.Domain;
+using Store.Services.Products.Repositories;
 
 namespace Store.Services.Products.Handlers
 {
     public class CreateProductHandler : ICommandHandler<CreateProduct>
     {
         private readonly IBusPublisher _busPublisher;
+        private readonly IProductRepository _productRepository;
 
-        public CreateProductHandler(IBusPublisher busPublisher)
+        public CreateProductHandler(IBusPublisher busPublisher,
+            IProductRepository productRepository)
         {
             _busPublisher = busPublisher;
+            _productRepository = productRepository;
         }
 
         public async Task HandleAsync(CreateProduct command, ICorrelationContext context)
         {
             Console.WriteLine($"Create product: {command.Name}.");
+            await _productRepository.AddAsync(new Product(command.Id,
+                command.Name, command.Category, command.Price));
             await _busPublisher.PublishEventAsync(new ProductCreated(command.Id,
                 command.Name, command.Category, command.Price), context);
         }
